674bd98d2976ac1502a8eeec2dc6a01ba03a3682
wb-hx347246 <wb-hx347246@alibaba-inc.com>
2018-05-16 21:26:49 +0800

    nettyDemo

---
+++ NioDemo/src/main/java/com/demo/netty/echosever/EchoSeverHandler.java
@@ -0,0 +1,40 @@
+package com.demo.netty.echosever;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * @Author wb-hx347246
+ * @Date 2018/5/16 10:10
+ */
+@ChannelHandler.Sharable
+public class EchoSeverHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf msg1 = (ByteBuf)msg;
+        System.out.println("echo server--received msg: "+msg1.toString());
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ NioDemo/src/main/java/com/demo/netty/sdf.java
@@ -0,0 +1,8 @@
+package com.demo.netty;
+
+/**
+ * @Author wb-hx347246
+ * @Date 2018/5/16 10:09
+ */
+public class sdf {
+}
