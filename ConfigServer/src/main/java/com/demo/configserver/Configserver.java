b5a529ddbabc8163d174955d3520eceb38ec9540
wb-hx347246 <wb-hx347246@alibaba-inc.com>
2018-09-20 14:51:53 +0800

    solve version confilit

---
+++ ConfigServer/src/main/java/com/demo/configserver/Configserver.java
@@ -0,0 +1,21 @@
+package com.demo.configserver;/**
+ * @Author:hexiao wb347246
+ * @Description
+ * @Date: $time$ $date$
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *@Author wb-hx347246
+ *@Date 2018/9/19 上午11:30
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Configserver {
+    public static void main(String[] args) {
+        SpringApplication.run(Configserver.class,args);
+    }
+}
